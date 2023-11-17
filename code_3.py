1e09927660b2ff2e6473bfe0366a9d78b05d884e
LikaShu <142459648+LikaShu@users.noreply.github.com>
2023-11-17 23:09:12 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,13 @@
+    public static void main(String[] args) {
+
+public class RandomDataGenerator {
+
+            data[i] = (int) (Math.random() * 100) + 1;
+    }
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Random Number: " + item);
+}
+        }
+        int[] data = new int[10];
+        for (int item : data) {
+        }
